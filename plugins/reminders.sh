475addd789b9710b8d1a17d8d99422c8d1d6fd45
Gleb Levkov <levkov.gleb.dev@gmail.com>
2025-09-09 13:47:52 +0300

    feat: add calendar current event

--- plugins/reminders.sh
+++ plugins/reminders.sh
@@ -1,3 +1,11 @@
 LABEL="$(reminders show-all --due-date today | awk -F': ' '{print $3}')"
+EVENT_NOW=$(icalBuddy -iep title,datetime -b "" --noCalendarNames --propertySeparators " | " eventsNow | head -n 1) 
 
-sketchybar --animate tanh 20 --set $NAME drawing=on label="$LABEL"
+if [[ -n "$EVENT_NOW" ]]; then 
+    IFS="|" read -r TITLE TIME <<<"$EVENT_NOW"
+
+    sketchybar --animate tanh 20 --set $NAME drawing=on label="$TITLE ($TIME)"
+else
+    sketchybar --animate tanh 20 --set $NAME drawing=on label="$LABEL"
+fi
+
