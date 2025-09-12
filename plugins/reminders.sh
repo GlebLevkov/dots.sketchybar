f46906885c24123d2225d89b72ab046a1c9300da
Gleb Levkov <levkov.gleb.dev@gmail.com>
2025-09-12 11:09:08 +0300

    feat: --excludeAllDayEvents for reminders

--- plugins/reminders.sh
+++ plugins/reminders.sh
@@ -1,5 +1,5 @@
 LABEL="$(reminders show-all --due-date today | awk -F': ' '{print $3}')"
-EVENT_NOW=$(icalBuddy -iep title,datetime -b "" --noCalendarNames --propertySeparators " | " eventsNow | head -n 1) 
+EVENT_NOW=$(icalBuddy -iep title,datetime -b "" --excludeAllDayEvents --noCalendarNames --propertySeparators " | " eventsNow | head -n 1) 
 
 if [[ -n "$EVENT_NOW" ]]; then 
     IFS="|" read -r TITLE TIME <<<"$EVENT_NOW"
