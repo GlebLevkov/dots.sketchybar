83782187257771749fa5121c8072b6eafc7fd778
Gleb Levkov <levkov.gleb.dev@gmail.com>
2025-09-12 11:08:24 +0300

    refactor: clock, weather

--- plugins/clock.sh
+++ plugins/clock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-sketchybar --set $NAME label="$(date '+%a %b %-d %-H:%M')"
+sketchybar --set $NAME icon="$(date '+%a %b %-d')" label="$(date +%H:%M)"
--- plugins/reminders.sh
+++ plugins/reminders.sh
@@ -4,8 +4,8 @@ EVENT_NOW=$(icalBuddy -iep title,datetime -b "" --noCalendarNames --propertySepa
 if [[ -n "$EVENT_NOW" ]]; then 
     IFS="|" read -r TITLE TIME <<<"$EVENT_NOW"
 
-    sketchybar --animate tanh 20 --set $NAME drawing=on label="$TITLE ($TIME)"
+    sketchybar --animate tanh 20 --set $NAME drawing=on label="$(echo "${TITLE:0:29}") ($TIME)"
 else
-    sketchybar --animate tanh 20 --set $NAME drawing=on label="$LABEL"
+    sketchybar --animate tanh 20 --set $NAME drawing=on label=$(echo "${LABEL:0:29}")
 fi
 
